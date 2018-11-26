02ed73bb2a0c4e05fb705a6551ceb2bc2dd19019
Slatius <slat.slatius@gmail.com>
2018-11-26 11:17:28 +0000

    Final

--- main.c
+++ main.c
@@ -11,8 +11,21 @@ int main (int argc, char **argv)
 	{
 		printf("Parametro %d: %s\n", i, argv[i]);
 	}
-
-
+	
+	FILE *a = abreFicheiro(argv[1], "r");
+	char *b;
+	int counter 		= 0,
+		fileHasntEnded  = 1;
+	do
+	{
+		b = lePalavra(a);
+		fileHasntEnded = !feof(a);
+		if (fileHasntEnded)
+			counter++;
+	}
+	while (fileHasntEnded);
+	
+	printf("Numero de palavras : %d", counter);
 
         return 0;
 }
